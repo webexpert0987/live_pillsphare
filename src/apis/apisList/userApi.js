7db97cacec0c94036b9d65a2251bb05151719eeb
webexpert0987 <webexpert0987@gmail.com>
2025-01-21 12:01:25 +0530

    fixed api issue

--- src/apis/apisList/userApi.js
+++ src/apis/apisList/userApi.js
@@ -1,13 +1,4 @@
-// import apiClient from '../api';
-import axios from 'axios';
+import apiClient from '../api';
 
-const apiClient = axios.create({
-//   baseURL: 'http://admin.pillsphere.com/wp-json/wp/v2',
-  // baseURL: process.env.REACT_APP_BACKEND_URL,
-  baseURL: '',
-  headers: {
-    'Content-Type': 'application/json',
-  },
-});
 export const loginUser = (data) => apiClient.post('/wp-json/wp/v2/login', data);
 export const registerUser = (data) => apiClient.post('/wp-json/wp/v2/register', data);
