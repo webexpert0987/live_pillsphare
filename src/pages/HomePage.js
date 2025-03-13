4dd1e0ce888bc7224828732ac58b9f7effdd2102
webexpert0987 <webexpert987@gmail.com>
2025-03-13 16:55:45 +0530

    added ConditionsWeTreat in home page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,24 +1,26 @@
 import { Box } from "@mui/material";
-import ServiceProvided from '../components/HomePage/serviceProvided';
+import ServiceProvided from "../components/HomePage/serviceProvided";
 import TreatmentSection from "../components/HomePage/treatmentSection";
 import DiscreetPackagingSection from "../components/HomePage/discreetPackaging";
 import WeeklyOffersSection from "../components/HomePage/weeklyOfferCategory";
 import HowItWorksSection from "../components/HomePage/HowItWorksSection";
 import FeaturedProducts from "../components/HomePage/featuredProduct/FeaturedProduct";
-import Carousel from '../components/HomePage/homeCarousel/carousel';
+import Carousel from "../components/HomePage/homeCarousel/carousel";
+import ConditionsWeTreat from "../components/OnlineClinic/ConditionsWeTreat";
 
 const HomePage = () => {
-    return(
-        <Box>
-            <Carousel/>
-            <ServiceProvided/>
-            <TreatmentSection/>
-            <FeaturedProducts/>
-            <DiscreetPackagingSection/>
-            <HowItWorksSection/>
-            <WeeklyOffersSection/>
-        </Box>
-    )
-}
+  return (
+    <Box>
+      <Carousel />
+      <ServiceProvided />
+      <ConditionsWeTreat />
+      {/* <TreatmentSection/> */}
+      <FeaturedProducts />
+      <DiscreetPackagingSection />
+      <HowItWorksSection />
+      <WeeklyOffersSection />
+    </Box>
+  );
+};
 
-export default HomePage
+export default HomePage;
