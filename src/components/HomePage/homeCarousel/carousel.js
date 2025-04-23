e5601c0b203de22747069816f2c51d62d5e39c28
webexpert0987 <webexpert987@gmail.com>
2025-04-23 18:54:49 +0530

    feat: add new banner images and update carousel component for improved navigation

--- src/components/HomePage/homeCarousel/carousel.js
+++ src/components/HomePage/homeCarousel/carousel.js
@@ -13,23 +13,27 @@ const Carousel = () => {
   const slides = [
     {
       id: 1,
-      text: "The website is currently under construction and is live only for the Gphc to review the application for new premises.",
-      img: "/carousel/Rectangle.png",
+      // text: "The website is currently under construction and is live only for the Gphc to review the application for new premises.",
+      img: "/images/banners/banner1.png",
+      link: "/online-clinic",
     },
     {
       id: 2,
-      text: "The website is currently under construction and is live only for the Gphc to review the application for new premises.",
-      img: "/carousel/Rectangle.png",
+      // text: "The website is currently under construction and is live only for the Gphc to review the application for new premises.",
+      img: "/images/banners/banner2.png",
+      link: "/shop",
     },
     {
       id: 3,
-      text: "The website is currently under construction and is live only for the Gphc to review the application for new premises.",
-      img: "/carousel/Rectangle.png",
+      // text: "The website is currently under construction and is live only for the Gphc to review the application for new premises.",
+      img: "/images/banners/banner3.png",
+      link: "/online-clinic/weight-loss",
     },
     {
       id: 4,
-      text: "The website is currently under construction and is live only for the Gphc to review the application for new premises.",
-      img: "/carousel/Rectangle.png",
+      // text: "The website is currently under construction and is live only for the Gphc to review the application for new premises.",
+      img: "/images/banners/banner4.png",
+      link: "/online-clinic/weight-loss",
     },
   ];
 
@@ -46,61 +50,80 @@ const Carousel = () => {
     <Box position="relative" width="100%">
       <Slider {...settings} ref={sliderRef}>
         {slides.map((slide) => (
-          <Box key={slide.id} textAlign="center" position={"relative"}>
+          <Box
+            key={slide.id}
+            textAlign="center"
+            position={"relative"}
+            sx={{ cursor: "pointer" }}
+          >
             <Box>
               <img
                 src={slide.img}
                 alt={slide.text}
-                style={{ width: "100%", minHeight: "280px", height: "auto", minWidth: "950px", maxWWidth: "100%" }}
+                style={{
+                  width: "100%",
+                  minHeight: "280px",
+                  height: "auto",
+                  minWidth: "950px",
+                  maxWWidth: "100%",
+                }}
+                onClick={(e) => {
+                  e.preventDefault();
+                  e.stopPropagation();
+                  // window.location.href = `${slide.link}`;
+                }}
               />
             </Box>
             {/*<Box sx={{position: 'absolute', top: '15%', left: {xs: '8.5%', lg: '13.5%'}, width: {sm:'90%', md:'450px'}, textAlign: 'left'}}>
               </Box></Box><Box width={{xs: '85%', lg: '530px'}}>*/}
-            <Box
-              sx={{
-                position: "absolute",
-                top: "15%",
-                left: { xs: "0", lg: "13.5%" },
-                width: { sm: "90%", md: "90%" },
-                textAlign: "left",
-                paddingLeft: { xs: "20px" },
-                paddingRight: { xs: "20px" },
-              }}
-            >
-              <Box width={{ xs: "100%", lg: "90%" }}>
-                <Box marginBottom={2}>
-                  <Typography
-                    sx={{
-                      fontSize: { sm: "22px", md: "30px", lg: "45px" },
-                      color: "#fff",
-                      fontWeight: 700,
-                      lineHeight: "1.2",
-                      whiteSpace: "break-spaces",
-                    }}
-                  >
-                    {slide.text}
-                  </Typography>
-                </Box>
-                {/* <Box>
-                  <Typography
-                    variant="h3"
-                    sx={{ lineHeight: "1.2", color: "#fff" }}
-                  >
-                    Use code PILLSMED at checkout. Discount code excludes
-                    Saxenda and weight loss bundles.
-                  </Typography>
-                </Box> */}
-                <Box>
-                <Link to="/shop" style={{ textDecoration: "none" }}>
-                  <CustomButton
-                    bgColor="tertiary.main"
-                    txColor="#fff"
-                    text="Shop Now"
-                  />
-                  </Link>
+            {slide.text && (
+              <Box
+                sx={{
+                  position: "absolute",
+                  top: "15%",
+                  left: { xs: "0", lg: "13.5%" },
+                  width: { sm: "90%", md: "90%" },
+                  textAlign: "left",
+                  paddingLeft: { xs: "20px" },
+                  paddingRight: { xs: "20px" },
+                }}
+              >
+                <Box width={{ xs: "100%", lg: "90%" }}>
+                  <Box marginBottom={2}>
+                    <Typography
+                      sx={{
+                        fontSize: { sm: "22px", md: "30px", lg: "45px" },
+                        color: "#fff",
+                        fontWeight: 700,
+                        lineHeight: "1.2",
+                        whiteSpace: "break-spaces",
+                      }}
+                    >
+                      {slide.text}
+                    </Typography>
+                  </Box>
+                  {/* <Box>
+                    <Typography
+                      variant="h3"
+                      sx={{ lineHeight: "1.2", color: "#fff" }}
+                    >
+                      Use code PILLSMED at checkout. Discount code excludes
+                      Saxenda and weight loss bundles.
+                    </Typography>
+                  </Box> */}
+                  <Box>
+                    <Link to="/shop" style={{ textDecoration: "none" }}>
+                      <CustomButton
+                        bgColor="tertiary.main"
+                        txColor="#fff"
+                        text="Shop Now"
+                      />
+                    </Link>
+                  </Box>
                 </Box>
               </Box>
-            </Box>
+            )}
+
             {/* <Box sx={{position: 'absolute', top: '66%', left: '10%', width: '450px'}}>
               Use code PILLSMED at checkout. Discount code excludes Saxenda and weight loss bundles.
             </Box>
@@ -120,7 +143,7 @@ const Carousel = () => {
         sx={{
           position: "absolute",
           right: { xs: "20px", md: "60px" },
-          bottom: { xs: "20px", md: "40px" },
+          bottom: { xs: "20px", md: "10px" },
         }}
       >
         {/* <Button onClick={() => sliderRef.current.slickPrev()} color="#fff"><Icon color="#fff" icon="solar:arrow-left-broken" width="24" height="24" /></Button> */}
